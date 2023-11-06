f69a9ce29a6c8fd3f536e0908ffd18ed822123c6
An <taennos1017@naver.com>
2023-11-06 20:52:01 +0900

    생성시간 jpa commit

--- src/main/java/com/example/forum/ForumApplication.java
+++ src/main/java/com/example/forum/ForumApplication.java
@@ -3,11 +3,15 @@ package com.example.forum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@EnableJpaAuditing // JPA Auditing 활성화
+@SpringBootApplication@SpringBootApplication
 public class ForumApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
+        SpringApplication.run(Application.class, args);
+    }
     }
 
 }
---
+++ src/main/java/com/example/forum/service/ForumService.java
@@ -0,0 +1,9 @@
+package com.example.forum.service;
+
+import com.example.forum.dto.PostRequestDto;
+import com.example.forum.dto.PostResponseDto;
+
+public class ForumService {
+    public PostResponseDto createPost(PostRequestDto requestDto) {
+    }
+}
